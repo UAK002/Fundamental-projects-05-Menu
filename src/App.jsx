d717af20dede43fd67781b47be800f1ca60d0ab8
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-04 02:55:59 +0530

    197. Menu - Unique Categories

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import Title from './Title';
 import menu from './data';
 import Menu from './Menu';
 
+// const tempCategories = menu.map((item) => item.category);
+// console.log(tempCategories);
+// const tempSet = new Set(tempCategories);
+// console.log(tempSet);
+// const tempItems = ['all', ...tempSet];
+// console.log(tempItems);
+
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))];
+// console.log(allCategories);
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+
+  const [categories, setCategories] = useState(allCategories);
   return (
     <main>
       <section className="menu">
